badf99a923b25e4b8c1de21c0b2fbbc747f4dee0
Erik Neves <erikrrn04@gmail.com>
2024-12-16 19:26:37 -0300

    set problem type

--- main.py
+++ main.py
@@ -27,14 +27,18 @@ OBJ_COEFFICIENTS = []
 RESTRICTIONS_COUNT = 0
 RESTRICTIONS_TYPE = []
 MATRIX = None # inclui o vetor b a direita
+IS_MAXIMIZATION = False
 
 def parseArgs():
     parser = argparse.ArgumentParser(description="Aplicação solução de problemas de programação linear usando o simplex")
     parser.add_argument("--input", type=str, required=True, help="Arquivo de entrada com os dados do problema.")
+    parser.add_argument("--decimals", type=int, required=False, default=3, help="Número de casas decimais para imprimir valores numéricos.")
+    parser.add_argument("--digits", type=int, required=False, default=7, help="Número total de dígitos para imprimir valores numéricos.")
+
     return parser.parse_args()
 
 def LoadPL(filename):
-    global VAR_COUNT, RESTRICTIONS_COUNT, VAR_TYPES, OBJ_COEFFICIENTS, RESTRICTIONS_TYPE, MATRIX
+    global VAR_COUNT, RESTRICTIONS_COUNT, VAR_TYPES, OBJ_COEFFICIENTS, RESTRICTIONS_TYPE, MATRIX, IS_MAXIMIZATION
 
     with open(filename, "r") as file:
         lines = file.readlines()
@@ -51,7 +55,9 @@ def LoadPL(filename):
         else:
             VAR_TYPES.append(TypesOfVariables.Free)
 
-    OBJ_COEFFICIENTS = np.array(list(map(int, lines[3].strip().split())))
+    objective = lines[3].strip().split()
+    OBJ_COEFFICIENTS = np.array(list(map(int, objective[1:])))
+    IS_MAXIMIZATION = objective[0] == "max"
 
     coef = []
     b = []
@@ -86,7 +92,7 @@ def ExtendTableau():
 
     return extended_matrix
 
-def PrintTableau(tableau, decimals=3, digits=7):
+def PrintTableau(tableau, decimals, digits):
     rows, cols = tableau.shape
     left_cols = rows - 1
     main_cols = cols - left_cols - 1
@@ -106,11 +112,19 @@ def PrintTableau(tableau, decimals=3, digits=7):
     print("\n".join(table))
 
 def main():
+    # Leitura dos parâmetros
     args = parseArgs()
 
+    # Carregandos os dados da PL nas variáveis globais
     LoadPL(args.input)
-    extended = ExtendTableau()
-    PrintTableau(extended, decimals=3, digits=7)
+    
+    # Removendo restrições com dependências linear
+    #MakeMatrixFullRank()
+
+    # Criando o tableau estendido
+    tableau = ExtendTableau()
+
+    PrintTableau(tableau, args.decimals, args.digits)
 
 if __name__ == "__main__":
     main() 
